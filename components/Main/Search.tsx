315214efce7b43ef009e9d0fa6bfd596526609cf
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-03 18:23:13 -0500

    💄 Update Search Component Structure

--- components/Main/Search.tsx
+++ components/Main/Search.tsx
@@ -1,24 +1,36 @@
 import Image from 'next/image'
+import { useState } from 'react'
 
 import SearchIcon from './SearchIcon'
 
 import closeIcon from '@icons/close.svg'
 
 const Search: React.FC = () => {
+  const [searching, setSearching] = useState(false)
+
+  const handleInputChange = () => {}
+
   return (
-    <section className="flex w-80 items-center rounded-3xl bg-background py-3 px-6">
-      <SearchIcon width={16} height={16} color="fill-gray" />
+    <label className="relative block ">
+      <span className="absolute inset-y-0 left-0 flex items-center pl-6">
+        <SearchIcon width={16} height={16} styles="fill-gray" />
+      </span>
       <input
         type="text"
         placeholder="Search in Web3Block"
-        className="ml-4 bg-transparent focus:outline-none"
+        className="block w-80 rounded-2xl bg-background py-2 pl-14 pr-14 shadow-sm placeholder:italic placeholder:text-gray focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+        onChange={handleInputChange}
       />
-      <button className="ml-auto rounded-full bg-sea p-2">
-        <div className="h-3 w-3">
-          <Image src={closeIcon} layout="responsive" />
-        </div>
-      </button>
-    </section>
+      {searching && (
+        <span className="absolute inset-y-0 right-0 flex items-center pr-4">
+          <button className="inline-block h-fit rounded-full bg-sea p-2">
+            <div className="h-3 w-3">
+              <Image src={closeIcon} layout="responsive" />
+            </div>
+          </button>
+        </span>
+      )}
+    </label>
   )
 }
 
