8b774f7bef5f20324987eea829dc2920fd38218e
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-03 11:50:25 -0500

    🎨 Tailwind Brand Pallete Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff8867',
+        secondary: {
+          DEFAULT: '#ff52ff',
+          200: '#ffcbff',
+          500: '#d339b3',
+        },
+        background: {
+          DEFAULT: '#F0F5F4',
+          light: '#f5f5f5',
+        },
+        sea: {
+          DEFAULT: '#00E1C9',
+          200: '#d1eeeb',
+          500: '#02a090',
+        },
+        purple: '#8d4ff6',
+        gray: {
+          DEFAULT: '#708A87',
+          dark: '#4f5857',
+          light: '#e2e8f0',
+        },
+      },
+    },
   },
   plugins: [],
 }
