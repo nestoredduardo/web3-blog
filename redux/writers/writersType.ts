5ff2edf9d3afc20e8911e68594ec758c7e065f5a
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-04 00:02:05 -0500

    🏷️Writer Slice Types

--- redux/writers/writersType.ts
+++ redux/writers/writersType.ts
@@ -1,5 +1,25 @@
-const GET_WRITERS = 'GET_WRITTERS'
-const GET_WRITERS_SUCCESS = 'GET_WRITTERS_SUCCESS'
-const GET_WRITERS_ERROR = 'GET_WRITTERS_ERROR'
+export enum ActionType {
+  GET_WRITERS = 'GET_WRITTERS',
+  GET_WRITERS_SUCCESS = 'GET_WRITTERS_SUCCESS',
+  GET_WRITERS_ERROR = 'GET_WRITTERS_ERROR',
+}
 
-export { GET_WRITERS, GET_WRITERS_SUCCESS, GET_WRITERS_ERROR }
+type GetWriterPayload = {
+  data: WriterPreview[]
+  total: number
+}
+
+interface actionGet {
+  type: ActionType.GET_WRITERS
+}
+
+interface actionSuccess {
+  type: ActionType.GET_WRITERS_SUCCESS
+  payload: GetWriterPayload
+}
+
+interface actionError {
+  type: ActionType.GET_WRITERS_ERROR
+}
+
+export type Action = actionGet | actionSuccess | actionError
