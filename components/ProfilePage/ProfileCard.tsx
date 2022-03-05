1c7450946061648310ab0d0589b7b204a6d1df1c
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-05 18:11:07 -0500

    💄 ProfileCard Styles

--- components/ProfilePage/ProfileCard.tsx
+++ components/ProfilePage/ProfileCard.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-import { formatName } from '@utils'
+import { formatName, formatDate } from '@utils'
 
 type ProfileCardProps = {
   writer: Writer
@@ -8,8 +8,8 @@ type ProfileCardProps = {
 
 const ProfileCard: React.FC<ProfileCardProps> = ({ writer }) => {
   return (
-    <section className="flex flex-col justify-center">
-      <div className="relative h-20 w-20">
+    <section className="flex flex-col items-center pb-6">
+      <div className="relative mt-5 h-20 w-20">
         <Image src={writer.picture} layout="fill" className="rounded-full" />
       </div>
       <h2 className="text-2xl font-medium">
@@ -18,38 +18,26 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ writer }) => {
       <ul className="mt-5 mb-6 flex flex-col justify-center">
         <li className="flex items-center">
           <div className="relative mr-2 h-4 w-4">
-            <Image
-              src="/icon/date.svg"
-              layout="fill"
-              className="rounded-full"
-            />
+            <Image src="/icon/date.svg" layout="fill" />
           </div>
           <p className="text-sm text-gray-dark">
-            Joined on {writer.registerDate}
+            Joined on {formatDate(writer.registerDate, 'profile')}
           </p>
         </li>
-        <li className="flex items-center">
+        <li className="my-2 flex items-center">
           <div className="relative mr-2 h-4 w-4">
-            <Image
-              src="/icon/email.svg"
-              layout="fill"
-              className="rounded-full"
-            />
+            <Image src="/icon/email.svg" layout="fill" />
           </div>
           <p className="text-sm text-gray-dark">{writer.email}</p>
         </li>
         <li className="flex items-center">
           <div className="relative mr-2 h-4 w-4">
-            <Image
-              src="/icon/location.svg"
-              layout="fill"
-              className="rounded-full"
-            />
+            <Image src="/icon/location.svg" layout="fill" />
           </div>
           <p className="text-sm text-gray-dark">{writer.location.country}</p>
         </li>
       </ul>
-      <button className="rounded-2xl  bg-gradient-to-r from-primary to-secondary px-7 py-2 font-semibold text-white">
+      <button className="flex items-center rounded-2xl  bg-gradient-to-r from-primary to-secondary px-7 py-3 font-semibold text-white">
         <div className="relative mr-2 h-5 w-5">
           <Image src="/icon/tip.svg" layout="fill" className="rounded-full" />
         </div>
