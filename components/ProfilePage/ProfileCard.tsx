7cd632e43011c9b6b7381c96514e03fc16f01984
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-05 09:06:29 -0500

    🧱 Profile file in Components

--- components/ProfilePage/ProfileCard.tsx
+++ components/ProfilePage/ProfileCard.tsx
@@ -1,55 +1,55 @@
-import Image from 'next/image';
+import Image from 'next/image'
 
-import { formatName } from '@utils';
+import { formatName } from '@utils'
 
-import dateIcon from '@icon/date.svg';
-import emailIcon from '@icon/email.svg';
-import locationIcon from '@icon/location.svg';
-import moneyIcon from '@icon/money.svg';
+import dateIcon from '@icon/date.svg'
+import emailIcon from '@icon/email.svg'
+import locationIcon from '@icon/location.svg'
+import moneyIcon from '@icon/money.svg'
 
 type ProfileCardProps = {
-  writer: Writer;
-};
+  writer: Writer
+}
 
 const ProfileCard: React.FC<ProfileCardProps> = ({ writer }) => {
   return (
     <section className="flex flex-col justify-center">
-      <div className="w-20 h-20">
+      <div className="h-20 w-20">
         <Image src={writer.picture} layout="fill" className="rounded-full" />
       </div>
-      <h2 className="font-medium text-2xl">
+      <h2 className="text-2xl font-medium">
         {formatName(writer.title, writer.firstName, writer.lastName)}
       </h2>
-      <ul className="flex flex-col justify-center mt-5 mb-6">
+      <ul className="mt-5 mb-6 flex flex-col justify-center">
         <li className="flex items-center">
-          <div className="w-4 h-4 mr-2">
+          <div className="mr-2 h-4 w-4">
             <Image src={dateIcon} layout="fill" className="rounded-full" />
           </div>
-          <p className="text-gray-dark text-sm">
+          <p className="text-sm text-gray-dark">
             Joined on {writer.registerDate}
           </p>
         </li>
         <li className="flex items-center">
-          <div className="w-4 h-4 mr-2">
+          <div className="mr-2 h-4 w-4">
             <Image src={emailIcon} layout="fill" className="rounded-full" />
           </div>
-          <p className="text-gray-dark text-sm">{writer.email}</p>
+          <p className="text-sm text-gray-dark">{writer.email}</p>
         </li>
         <li className="flex items-center">
-          <div className="w-4 h-4 mr-2">
+          <div className="mr-2 h-4 w-4">
             <Image src={locationIcon} layout="fill" className="rounded-full" />
           </div>
-          <p className="text-gray-dark text-sm">{writer.location.country}</p>
+          <p className="text-sm text-gray-dark">{writer.location.country}</p>
         </li>
       </ul>
       <button className="rounded-2xl  bg-gradient-to-r from-primary to-secondary px-7 py-2 font-semibold text-white">
-        <div className="w-5 h-5 mr-2">
+        <div className="mr-2 h-5 w-5">
           <Image src={moneyIcon} layout="fill" className="rounded-full" />
         </div>
         Send tip to creator
       </button>
     </section>
-  );
-};
+  )
+}
 
-export default ProfileCard;
+export default ProfileCard
