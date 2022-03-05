6425f810cbc20c9acfef40b377f5f06acb5eec5a
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-04 23:45:30 -0500

    🏷️Writer,Location,PostPreview Types

--- index.d.ts
+++ index.d.ts
@@ -5,12 +5,44 @@ type ListResponse = {
   limit: number
 }
 
+type Writer = {
+  id: string
+  title: string
+  firstName: string
+  lastName: string
+  gender: string
+  email: string
+  dateOfBirth: string
+  registerDate: string
+  phone: string
+  picture: string
+  location: Location
+}
+
+type Location = {
+  street: string
+  city: string
+  state: string
+  country: string
+  timezone: string
+}
+
 type WriterPreview = {
   id: string
   title: string
   firstName: string
   lastName: string
   picture: string
+}
+
+type PostPreview = {
+  id: string
+  text: string
+  image: string
+  likes: number
+  tags: string[]
+  publishDate: string
+  owner: object
 }
 
 type WritersState = {
