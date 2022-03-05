c85b87dccd0fcb52aa790753f7dbc2d61357d28c
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-05 15:37:03 -0500

    💫 Writers Endpoint

---
+++ pages/api/writers/index.ts
@@ -0,0 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { getAllWriters } from '../utils'
+
+const handleSearch = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const allWriters = await getAllWriters()
+
+    res.status(200).json(allWriters)
+  } catch (error) {
+    res.status(404).json({ message: 'RIP Server' })
+  }
+}
+
+export default handleSearch
