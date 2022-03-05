3d05eb271c776167a99a58f4f20d6d99501f3029
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-05 18:07:52 -0500

    👔 Format Date Util

--- utils/index.ts
+++ utils/index.ts
@@ -5,4 +5,32 @@ const formatName = (title: string, firstName: string, lastName: string) => {
   return `${titleUppercase}. ${firstName} ${lastName}`
 }
 
-export { formatName }
+const formatDate = (dateString: string, type: string) => {
+  const date = new Date(dateString)
+  let options: Intl.DateTimeFormatOptions = {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  }
+  date.toLocaleDateString('en-US', options)
+
+  if (type == 'profile') {
+    return (
+      date.getDate() +
+      ' ' +
+      date.toLocaleString('en-us', { month: 'short' }) +
+      ' ' +
+      date.getFullYear()
+    )
+  } else if (type == 'blog') {
+    return (
+      date.toLocaleString('en-us', { month: 'short' }) +
+      ' ' +
+      date.getDate() +
+      ' ' +
+      date.getFullYear()
+    )
+  }
+}
+
+export { formatName, formatDate }
