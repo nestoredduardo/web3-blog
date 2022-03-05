5c2894fd39d635b7d86658ea366fdc91a0d80eb4
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-05 09:48:40 -0500

    ✨ Go to WriterProfile when click WriterItem

--- components/Main/Writters/WriterItem.tsx
+++ components/Main/Writters/WriterItem.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { formatName } from '@utils'
 
 type WriterItemProps = {
@@ -6,19 +8,21 @@ type WriterItemProps = {
 
 const WriterItem: React.FC<WriterItemProps> = ({ writer }) => {
   return (
-    <li className="my-2 flex w-80 items-center rounded-xl border-2 border-gray-light py-4 px-6">
-      <div className="mr-4 h-11 w-11">
-        <img src={writer.picture} className="rounded-full" />
-      </div>
-      <div>
-        <h3 className="text-lg font-medium">
-          {formatName(writer.title, writer.firstName, writer.lastName)}
-        </h3>
-        <p className="font-medium text-gray-dark">
-          {'@' + writer.firstName.toLowerCase()}
-        </p>
-      </div>
-    </li>
+    <Link href={`/${writer.id}`}>
+      <li className="bounce mx-10 my-2 flex w-80 cursor-pointer items-center rounded-xl border-2 border-gray-light py-4 px-6 hover:bg-gray-light">
+        <div className="mr-4 h-11 w-11">
+          <img src={writer.picture} className="rounded-full" />
+        </div>
+        <div>
+          <h3 className="text-lg font-medium">
+            {formatName(writer.title, writer.firstName, writer.lastName)}
+          </h3>
+          <p className="font-medium text-gray-dark">
+            {'@' + writer.firstName.toLowerCase()}
+          </p>
+        </div>
+      </li>
+    </Link>
   )
 }
 
