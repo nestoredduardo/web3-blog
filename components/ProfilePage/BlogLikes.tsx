68da9d08f108dfd791013dd92aa191b81c870ace
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-05 19:35:14 -0500

    💫 Like Component and Animation

---
+++ components/ProfilePage/BlogLikes.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+const BlogLikes: React.FC<{ likes: number }> = ({ likes }) => {
+  const [isLiked, setIsLiked] = useState(false)
+
+  return (
+    <div
+      className="relative ml-auto flex cursor-pointer items-center"
+      onClick={() => setIsLiked(!isLiked)}
+    >
+      <div className={'heart ' + (isLiked && 'heart-animation')}></div>
+      <p
+        className={
+          'whitespace-nowrap text-sm text-gray-dark ' +
+          (isLiked && 'text-red-500')
+        }
+      >
+        {isLiked ? likes + 1 : likes} likes
+      </p>
+    </div>
+  )
+}
+
+export default BlogLikes
