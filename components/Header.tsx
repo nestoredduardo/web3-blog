e1471b1b42e20c39f19d58c8365d16957ee694bc
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-06 15:26:57 -0500

    📱 Header Responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,13 +7,15 @@ import logo from '@images/logo.svg'
 
 const Header: React.FC = () => {
   return (
-    <header className="flex justify-between border-b-2  border-gray-light px-3 py-4">
+    <header className="flex justify-between border-b-2  border-gray-light px-3 py-4 md:px-8 xl:px-16">
       <Link href="/">
         <section className="flex cursor-pointer items-center gap-2">
           <div className="h-10 w-10">
             <Image src={logo} layout="responsive" />
           </div>
-          <h1 className="text-lg font-bold">Web3Block</h1>
+          <h1 className="text-lg font-bold lg:text-xl xl:text-2xl">
+            Web3Block
+          </h1>
         </section>
       </Link>
       <Button styles="px-7 py-2">Connect Wallet</Button>
