fb9e428ecb0ff0cc2f079744d55efd96c5f0b3ed
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-06 15:33:41 -0500

    📱 Home Title Responsive

--- components/Main/Title.tsx
+++ components/Main/Title.tsx
@@ -1,6 +1,6 @@
 const Title: React.FC = () => {
   return (
-    <h2 className="mt-16 mb-11 w-64 text-center text-4xl font-bold">
+    <h2 className="mt-16 mb-11 w-64 text-center text-4xl font-bold md:w-80 md:text-5xl lg:w-[65%] xl:w-[700px]">
       Find the best{' '}
       <span className="bg-gradient-to-br from-secondary to-primary bg-clip-text text-transparent">
         Web3 Writers
