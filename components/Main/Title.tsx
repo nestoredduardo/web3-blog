73d9253c5c8e6d18515d70d813368b2164fc9bf5
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-06 15:40:18 -0500

    📱 Home Title xl bigger text

--- components/Main/Title.tsx
+++ components/Main/Title.tsx
@@ -1,6 +1,6 @@
 const Title: React.FC = () => {
   return (
-    <h2 className="mt-16 mb-11 w-64 text-center text-4xl font-bold md:w-80 md:text-5xl lg:w-[65%] xl:w-[700px]">
+    <h2 className="mt-16 mb-11 w-64 text-center text-4xl font-bold md:w-80 md:text-5xl lg:w-[65%] xl:w-[800px] xl:text-6xl">
       Find the best{' '}
       <span className="bg-gradient-to-br from-secondary to-primary bg-clip-text text-transparent">
         Web3 Writers
