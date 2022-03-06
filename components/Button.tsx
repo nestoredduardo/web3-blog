ac1c08b73acc57a5fd98d01367cd3bc8d0faf7f2
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-06 15:17:51 -0500

    💄 Show Info when click Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,17 @@
+import { toast } from 'react-toastify'
+
 type ButtonProps = {
   styles?: string
 }
 
 const Button: React.FC<ButtonProps> = ({ children, styles }) => {
+  const notify = () => {
+    toast('Comming soon...Little Rockstar 🦄')
+  }
+
   return (
     <button
+      onClick={notify}
       className={
         'rounded-2xl bg-gradient-to-r from-primary to-secondary font-semibold text-white hover:animate-bg-animation hover:bg-[length:200%_200%] hover:text-black ' +
         styles
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import '../styles/magic.css'
-
+import 'react-toastify/dist/ReactToastify.min.css'
 import type { AppProps } from 'next/app'
 
-import generateStore from '@store'
 import { Provider } from 'react-redux'
 import NextNProgress from 'nextjs-progressbar'
+import { ToastContainer } from 'react-toastify'
 
+import generateStore from '@store'
 import Layout from '@components/Layout'
 
 const store = generateStore()
@@ -15,6 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <NextNProgress color="#00E1C9" options={{ showSpinner: false }} />
+      <ToastContainer />
       <Layout>
         <Component {...pageProps} />
       </Layout>
