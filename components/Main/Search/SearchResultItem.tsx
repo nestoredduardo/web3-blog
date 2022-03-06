299fba5ba40bed09fe4d89201fef8b49b7978bc4
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-06 15:58:51 -0500

    📱 Search Results Fix Responsive

--- components/Main/Search/SearchResultItem.tsx
+++ components/Main/Search/SearchResultItem.tsx
@@ -10,11 +10,11 @@ const WriterItem: React.FC<WriterItemProps> = ({ writer }) => {
   return (
     <Link href={`/${writer.id}`}>
       <li className="flex cursor-pointer items-center py-4 px-6 hover:bg-gray-light">
-        <div className="mr-4 h-11 w-11">
+        <div className="mr-4 h-11 w-11 lg:h-14 lg:w-14 xl:h-16 xl:w-16">
           <img src={writer.picture} className="rounded-full" />
         </div>
         <div>
-          <h3 className="text-lg font-medium">
+          <h3 className="text-lg font-medium xl:text-xl">
             {formatName(writer.title, writer.firstName, writer.lastName)}
           </h3>
           <p className="font-medium text-gray-dark">
