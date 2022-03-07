221972b56c5e197c23fd84c681d1df20fc21360b
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-07 11:08:39 -0500

    🧱 Reusable Button with Onclick prop

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,19 +1,14 @@
-import { toast } from 'react-toastify'
-
 type ButtonProps = {
   styles?: string
+  onClick: React.MouseEventHandler<HTMLButtonElement>
 }
 
-const Button: React.FC<ButtonProps> = ({ children, styles }) => {
-  const notify = () => {
-    toast('Coming soon...Little Rockstar 🦄')
-  }
-
+const Button: React.FC<ButtonProps> = ({ children, styles, onClick }) => {
   return (
     <button
-      onClick={notify}
+      onClick={onClick}
       className={
-        'rounded-2xl bg-gradient-to-r from-primary to-secondary font-semibold text-white hover:animate-bg-animation hover:bg-[length:200%_200%] hover:text-black ' +
+        'rounded-2xl bg-gradient-to-r from-primary to-secondary py-2 px-4 font-semibold text-white hover:animate-bg-animation hover:bg-[length:200%_200%] hover:text-black lg:text-lg ' +
         styles
       }
     >
