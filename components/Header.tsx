5c127e895a556a43fc451d93a8c69e0df016eac6
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-07 11:24:19 -0500

    💄 Update Header py to height

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import Button from '@components/Button'
+import Connect2Phantom from '@components/Connect2Phantom'
 
 import logo from '@images/logo.svg'
 
 const Header: React.FC = () => {
   return (
-    <header className="flex justify-between border-b-2  border-gray-light px-3 py-4 md:px-8 xl:px-16">
+    <header className="flex h-[74px] justify-between border-b-2 border-gray-light px-3 md:px-8 xl:px-16">
       <Link href="/">
         <section className="flex cursor-pointer items-center gap-2">
           <div className="h-10 w-10">
@@ -18,7 +19,8 @@ const Header: React.FC = () => {
           </h1>
         </section>
       </Link>
-      <Button styles="px-7 py-2">Connect Wallet</Button>
+      {/* <Button styles="px-7 py-2">Connect Wallet</Button> */}
+      <Connect2Phantom />
     </header>
   )
 }
