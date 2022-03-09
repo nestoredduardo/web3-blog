1443f33bfe160c5537c53808ec7126d2044b153e
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-08 20:39:51 -0500

    🩹 Fix utils import

--- pages/api/fetchWriters.ts
+++ pages/api/fetchWriters.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { fetchWriters } from './utils'
+import { fetchWriters } from '@utils/fetchWriters'
 
 const handleFetchWriters = async (
   req: NextApiRequest,
