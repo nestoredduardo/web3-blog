11ece0c7bb5689fffbaedf1a1cbe0741c4009adb
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-08 20:03:07 -0500

    ✨ Push to HomePage in yourNFTs when wallet isnt connected

--- components/YourNFTs/index.tsx
+++ components/YourNFTs/index.tsx
@@ -1,5 +1,27 @@
-const YourWallet: React.FC = () => {
-  return <h1>Watch your NFTs</h1>
+import { connect, ConnectedProps } from 'react-redux'
+
+import { RootState } from '@store'
+import { useRouter } from 'next/router'
+import { toast } from 'react-toastify'
+
+const YourWallet: React.FC<PropsFromRedux> = ({ publicAddress }) => {
+  if (!publicAddress) {
+    const router = useRouter()
+    router.push('/')
+  } else {
+    return <h1>Watch your NFTs</h1>
+  }
+
+  return null
 }
 
-export default YourWallet
+const mapStateToProps = (state: RootState) => {
+  return {
+    publicAddress: state.user.publicAddress,
+  }
+}
+
+const connector = connect(mapStateToProps, null)
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+export default connector(YourWallet)
