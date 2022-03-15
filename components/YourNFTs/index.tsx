c10e6dcc0abd93118df939b26edd78cff9db5fd8
Nestor Mamani <nmamanipantoja@gmail.com>
2022-03-15 12:57:24 -0500

    🐛 Fix useRouter on server

--- components/YourNFTs/index.tsx
+++ components/YourNFTs/index.tsx
@@ -1,26 +1,18 @@
 import { connect, ConnectedProps } from 'react-redux'
 
 import { RootState } from '@store'
-import { useRouter } from 'next/router'
 
 import NFTList from './NFTList'
 
 const YourWallet: React.FC<PropsFromRedux> = ({ publicAddress }) => {
-  if (!publicAddress) {
-    const router = useRouter()
-    router.push('/')
-  } else {
-    return (
-      <main className="px-3 md:px-8 xl:px-40">
-        <h1 className="mt-6 mb-4 text-center text-3xl font-semibold">
-          Watch your NFTs
-        </h1>
-        <NFTList />
-      </main>
-    )
-  }
-
-  return <h1>Returning to HomePage...</h1>
+  return (
+    <main className="px-3 md:px-8 xl:px-40">
+      <h1 className="mt-6 mb-4 text-center text-3xl font-semibold">
+        Watch your NFTs
+      </h1>
+      <NFTList />
+    </main>
+  )
 }
 
 const mapStateToProps = (state: RootState) => {
